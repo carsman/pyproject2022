84b59230509bab15ebfabaa6de872b9ed3e3e9bf
carsman <nikita.jeanpaul@gmail.com>
2022-10-16 16:00:30 +0300

    v3

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-import telebot
+import telebot, re, wikipedia
 from convert import *
-from functions import *
 from telebot import types
 bot = telebot.TeleBot('5403325384:AAHEje2j4h1ERaK5Dk5yry5BGBzT_lY59zA')
 
+
+@bot.message_handler(commands=["wiki"])
+def wiki(message, res=False):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add(types.KeyboardButton("Millimetre"))
+    markup.add(types.KeyboardButton("Centimetre"))
+    markup.add(types.KeyboardButton("Decimetre"))
+    markup.add(types.KeyboardButton("Metre"))
+    markup.add(types.KeyboardButton("Kilometre"))
+    markup.add(types.KeyboardButton("Mile"))
+    markup.add(types.KeyboardButton("Foot"))
+    markup.add(types.KeyboardButton("Inch"))
+    markup.add(types.KeyboardButton("Yard"))
+    markup.add(types.KeyboardButton("Gram"))
+    markup.add(types.KeyboardButton("Kilogram"))
+    markup.add(types.KeyboardButton("Ounce"))
+    markup.add(types.KeyboardButton("Celsius"))
+    markup.add(types.KeyboardButton("Kelvin"))
+    markup.add(types.KeyboardButton("Fahrenheit"))
+    bot.send_message(message.chat.id, 'What info do you want to get?', reply_markup=markup)
+    bot.register_next_step_handler(message, wiki_choosing)
+
+
+def wiki_choosing(message):
+    markup = types.ReplyKeyboardRemove(selective=False)
+    bot.send_message(message.chat.id, getwiki(message.text), reply_markup=markup)
+    bot.send_message(message.chat.id, 'Press /wiki to learn about other units or /convert to make a convert')
+
 @bot.message_handler(commands=["convert"])
 def convert(message, res=False):
-        # Добавляем две кнопки
-        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         item1 = types.KeyboardButton("🌡Temp")
         item2 = types.KeyboardButton("⚖Weight")
-        item3 = types.KeyboardButton("📐Lenght")
+        item3 = types.KeyboardButton("📐length")
         markup.add(item1)
         markup.add(item2)
         markup.add(item3)
@@ -19,15 +45,16 @@ def convert(message, res=False):
 
 
 def type_choosing(message):
+    markup = types.ReplyKeyboardRemove(selective=False)
     if message.text == "🌡Temp":
-        bot.send_message(message.chat.id, "Write temperature do you want to convert")
+        bot.send_message(message.chat.id, "Write temperature do you want to convert", reply_markup=markup)
         bot.register_next_step_handler(message, get_initial_temp)
     elif message.text == "⚖Weight":
-        bot.send_message(message.chat.id, "Write weight do you want to convert")
+        bot.send_message(message.chat.id, "Write weight do you want to convert", reply_markup=markup)
         bot.register_next_step_handler(message, get_initial_weight)
 
     elif message.text == "📐length":
-        bot.send_message(message.chat.id, "Write length do you want to convert")
+        bot.send_message(message.chat.id, "Write length do you want to convert", reply_markup=markup)
         bot.register_next_step_handler(message, get_initial_length)
 
 
@@ -44,7 +71,8 @@ def get_temp(message):
     bot.send_message(message.chat.id, "Converting")
     n, var1 = question.split(" ")
     n = int(n)
-    bot.send_message(message.chat.id, "Answer: " + str(temperature(n, var1, newinch)) + newinch)
+    bot.send_message(message.chat.id, "Answer: " + str(temperature(n, var1.lower(), newinch.lower())) + newinch)
+    bot.send_message(message.chat.id, 'Press /wiki to learn about units or /convert to make a new convert')
 
 
 def get_initial_weight(message):
@@ -60,7 +88,8 @@ def get_weight(message):
     bot.send_message(message.chat.id, "Converting")
     n, var1 = question.split(" ")
     n = int(n)
-    bot.send_message(message.chat.id, "Answer: " + str(temperature2(n, var1, newinch)) + newinch)
+    bot.send_message(message.chat.id, "Answer: " + str(weight(n, var1.lower(), newinch.lower())) + newinch)
+    bot.send_message(message.chat.id, 'Press /wiki to learn about units or /convert to make a new convert')
 
 
 def get_initial_length(message):
@@ -76,7 +105,29 @@ def get_length(message):
     bot.send_message(message.chat.id, "Converting")
     n, var1 = question.split(" ")
     n = int(n)
-    bot.send_message(message.chat.id, "Answer: " + str(temperature2(n, var1, newinch)) + newinch)
+    bot.send_message(message.chat.id, "Answer: " + str(length(n, var1.lower(), newinch.lower())) + newinch)
+    bot.send_message(message.chat.id, 'Press /wiki to learn about units or /convert to make a new convert')
+
+
+def getwiki(s):
+    try:
+        ny = wikipedia.page(s)
+        wikitext=ny.content[:1000]
+        wikimas=wikitext.split('.')
+        wikimas = wikimas[:-1]
+        wikitext2 = ''
+        for x in wikimas:
+            if not('==' in x):
+                if(len((x.strip()))>3):
+                   wikitext2=wikitext2+x+'.'
+            else:
+                break
+        wikitext2=re.sub('\([^()]*\)', '', wikitext2)
+        wikitext2=re.sub('\([^()]*\)', '', wikitext2)
+        wikitext2=re.sub('\{[^\{\}]*\}', '', wikitext2)
+        return wikitext2
+    except Exception as e:
+        return 'В энциклопедии нет информации об этом'
 
 
 @bot.message_handler(commands=["start"])
@@ -85,6 +136,4 @@ def start(m, res=False):
     bot.send_message(m.chat.id, 'To convert something, please write /convert')
 
 
-
-# Запускаем бота
 bot.polling(none_stop=True, interval=0)
